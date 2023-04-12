d23fa03478021e2f031bd1516b99b599a4e33a81
kangjf1943 <kangjf1943@gmail.com>
2023-04-12 16:54:16 +0900

    fix: update user information

--- main.R
+++ main.R
@@ -1,13 +1,16 @@
 #パッケージのインストール
+library(dplyr)
+library(rvest)
 library(slackr)
 
 # setup
 my_channel <- "#test"
-my_token <- "xoxb-5095796214786-5081358760999-p2T8mCEQ3rkl1WBvR6WrxDzq"
+my_token <- "xoxb-5095796214786-5081358760999-5hHG9xKO5BoJ0RFqsdJQuy8n"
 my_webhook_url <- paste0(
   "https://hooks.slack.com/services/", 
-  "T052TPE6AP4/B052TTTLARZ/GGlMznwnefQAMR9mNONU07pv"
+  "T052TPE6AP4/B052UBRSCF4/DJdr4cU3i1IpLr0yXteWqk2I"
 )
+# bug: does webhook url expire? 
 
 slackr_setup(
   channel = my_channel, 
@@ -31,6 +34,6 @@ slackr_msg(title)
 
 # future potential features: 
 # keep interested articles based on abstract text mining
-# update titles when new issue is published
+# update titles when new issue is published - use cloud R?
 # add other info to the title list, e.g., links to the articles 
 # get highlight points based on GPT? 
